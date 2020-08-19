3f47fc9a6446541d06b7f8f52d754d335810750f
Lars Eckart <lars.eckart@gmail.com>
2020-08-19 19:37:31 +0300

    update to today

--- src/main/java/bottles/Bottles.java
+++ src/main/java/bottles/Bottles.java
@@ -2,15 +2,15 @@ package bottles;
 
 public class Bottles {
 
-    public String verse(int i) {
-        throw new UnsupportedOperationException();
-    }
+  public String verse(int i) {
+    throw new UnsupportedOperationException();
+  }
 
-    public String verses(int i, int i1) {
-        throw new UnsupportedOperationException();
-    }
+  public String verses(int i, int i1) {
+    throw new UnsupportedOperationException();
+  }
 
-    public String song() {
-        throw new UnsupportedOperationException();
-    }
+  public String song() {
+    throw new UnsupportedOperationException();
+  }
 }
