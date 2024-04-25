5c74f909e3f4d86db11c15edde97bf96223e59d8
Lars Eckart <lars.eckart@hey.com>
2024-04-25 10:28:14 +0200

    . r Rename

--- src/main/java/bottles/Bottles.java
+++ src/main/java/bottles/Bottles.java
@@ -2,9 +2,9 @@ package bottles;
 
 public class Bottles {
 
-  public String verse(int n) {
-    return n + " bottles of beer on the wall, " + n + " bottles of beer.\n" +
-           "Take one down and pass it around, " + (n - 1) + " bottles of beer on the wall.\n";
+  public String verse(int amountOfBottles) {
+    return amountOfBottles + " bottles of beer on the wall, " + amountOfBottles + " bottles of beer.\n" +
+           "Take one down and pass it around, " + (amountOfBottles - 1) + " bottles of beer on the wall.\n";
   }
 
   public String verses(int upper, int lower) {
