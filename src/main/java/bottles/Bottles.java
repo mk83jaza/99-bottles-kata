be5c9829036f18ae9b4e91818e6575e180f46221
Lars Eckart <lars.eckart@hey.com>
2024-04-25 11:01:41 +0200

    genius refactoring

--- src/main/java/bottles/Bottles.java
+++ src/main/java/bottles/Bottles.java
@@ -3,13 +3,15 @@ package bottles;
 public class Bottles {
 
   public String verse(int amountOfBottles) {
-    if (amountOfBottles == 0) {
-      return "No more bottles of beer on the wall, no more bottles of beer.\n"
-             + "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
-    }
+    // if (amountOfBottles == 0) {
+    //   return "No more bottles of beer on the wall, no more bottles of beer.\n"
+    //          + "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+    // }
     int nextAmountOfBottles = amountOfBottles - 1;
     String result = "";
-    if (amountOfBottles == 1) {
+    if (amountOfBottles == 0) {
+      result += "No more bottles of beer on the wall, no more bottles of beer.\n";
+    } else if (amountOfBottles == 1) {
       result += amountOfBottles + " bottle of beer on the wall, " + amountOfBottles + " bottle of beer.\n";
       result += "Take it down and pass it around, no more";
     } else {
@@ -18,6 +20,8 @@ public class Bottles {
     }
     if (nextAmountOfBottles == 1) {
       result += " bottle of beer on the wall.\n";
+    } else if (nextAmountOfBottles == -1) {
+      result += "Go to the store and buy some more, 99 bottles of beer on the wall.";
     } else {
       result += " bottles of beer on the wall.\n";
     }
