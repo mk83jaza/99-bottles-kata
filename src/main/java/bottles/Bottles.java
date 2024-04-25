c28ae04494e89bb6c99ee51d4457d7aa6235fe81
Lars Eckart <lars.eckart@hey.com>
2024-04-25 11:01:53 +0200

    . r Delete Clutter

--- src/main/java/bottles/Bottles.java
+++ src/main/java/bottles/Bottles.java
@@ -3,19 +3,17 @@ package bottles;
 public class Bottles {
 
   public String verse(int amountOfBottles) {
-    // if (amountOfBottles == 0) {
-    //   return "No more bottles of beer on the wall, no more bottles of beer.\n"
-    //          + "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
-    // }
     int nextAmountOfBottles = amountOfBottles - 1;
     String result = "";
     if (amountOfBottles == 0) {
       result += "No more bottles of beer on the wall, no more bottles of beer.\n";
     } else if (amountOfBottles == 1) {
-      result += amountOfBottles + " bottle of beer on the wall, " + amountOfBottles + " bottle of beer.\n";
+      result += amountOfBottles + " bottle of beer on the wall, " + amountOfBottles
+                + " bottle of beer.\n";
       result += "Take it down and pass it around, no more";
     } else {
-      result += amountOfBottles + " bottles of beer on the wall, " + amountOfBottles + " bottles of beer.\n";
+      result += amountOfBottles + " bottles of beer on the wall, " + amountOfBottles
+                + " bottles of beer.\n";
       result += "Take one down and pass it around, " + nextAmountOfBottles;
     }
     if (nextAmountOfBottles == 1) {
