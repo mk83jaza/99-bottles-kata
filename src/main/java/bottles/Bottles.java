6bc6abcd07066f57331e0495face8decc4a18707
Lars Eckart <lars.eckart@hey.com>
2024-04-25 10:37:36 +0200

    . r Extract Variable

--- src/main/java/bottles/Bottles.java
+++ src/main/java/bottles/Bottles.java
@@ -3,10 +3,11 @@ package bottles;
 public class Bottles {
 
   public String verse(int amountOfBottles) {
+    int nextAmountOfBottles = amountOfBottles - 1;
     String result =
         amountOfBottles + " bottles of beer on the wall, " + amountOfBottles + " bottles of beer.\n"
-        + "Take one down and pass it around, " + (amountOfBottles - 1);
-    if (amountOfBottles - 1 == 1) {
+        + "Take one down and pass it around, " + nextAmountOfBottles;
+    if (nextAmountOfBottles == 1) {
       result += " bottle of beer on the wall.\n";
     } else {
       result += " bottles of beer on the wall.\n";
